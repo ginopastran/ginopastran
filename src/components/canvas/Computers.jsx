d2e7c0e18a6c31d76b91d89d1f58b25f08e1fa3d
ginopastran <ginopastran@gmail.com>
2023-09-29 18:32:26 -0300

    fixing issues

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,16 +9,16 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
-        angle={120}
+        angle={0.12}
         penumbra={1}
-        intensity={1000}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={2} />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
